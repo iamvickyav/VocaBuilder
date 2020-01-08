dda3e92921d540fd222036e776fe46a8ebffb43d
iamvickyav <vickyavw.10@gmail.com>
2020-01-08 23:47:27 +0530

    Change in title size & font. Title bar hidden

--- app/src/main/java/com/iamvickyav/vocabuilder/MainActivity.java
+++ app/src/main/java/com/iamvickyav/vocabuilder/MainActivity.java
@@ -3,6 +3,7 @@ package com.iamvickyav.vocabuilder;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
 
         addButton = findViewById(R.id.addButton);
