aac0332840dc3406436b5100802c14cd8d3cda9c
iamvickyav <vickyavw.10@gmail.com>
2020-01-17 23:14:49 +0530

    Added settings in home page menu bar

--- app/src/main/java/com/iamvickyav/vocabuilder/MainActivity.java
+++ app/src/main/java/com/iamvickyav/vocabuilder/MainActivity.java
@@ -2,8 +2,9 @@ package com.iamvickyav.vocabuilder;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,8 +16,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getSupportActionBar().hide();
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
 
         addButton = findViewById(R.id.addButton);
@@ -47,5 +48,25 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.settings_menu) {
+           /* Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);*/
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
